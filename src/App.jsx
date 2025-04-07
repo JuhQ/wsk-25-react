9fe8d0bc5978bf49077816027f6e632f26fc4603
Juha Tauriainen <juha@bin.fi>
2025-04-07 10:26:00 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+// react komponentin voi luoda usealla tavalla
+// const App = function() {
+// const App = () => {
+function App() {
+  return (
+    <>
+      <h1>My App</h1>
+    </>
+  );
+}
+
+export default App;
