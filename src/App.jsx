fd0455f8dc0bc778ed1f85330a28541ea216c6e3
Juha Tauriainen <juha@bin.fi>
2025-04-07 11:49:13 +0300

    Add greeting component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css';
 
+import Greeting from './Greeting';
+
 // react komponentin voi luoda usealla tavalla
 // const App = function() {
 // const App = () => {
 function App() {
   return (
     <>
+      <Greeting name="Ilkka" />
+      <Greeting name={123} />
+      <Greeting name="Heikki" />
       <h1>My App</h1>
+      <Greeting name="Tiina" />
+      <Greeting name="Maija" />
     </>
   );
 }
