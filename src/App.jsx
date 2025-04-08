69455d177af372b22d4262711721432c43e34c07
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2025-04-08 11:26:32 +0300

    Implement routing for Single view and enhance Single component with media display; update MediaRow to use Link for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import Home from './views/Home';
 import Layout from './components/Layout';
 import Profile from './views/Profile';
 import Upload from './views/Upload';
+import Single from './views/Single';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/upload" element={<Upload />} />
+          <Route path="/single" element={<Single />} />
+          <Route path="/" element={<Home />} />
         </Route>
       </Routes>
     </BrowserRouter>
