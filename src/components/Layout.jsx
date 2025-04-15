c07ca327a58c603c87894b88b40085def33a75db
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2025-04-15 11:33:27 +0300

    Refactor Layout component to include user state for conditional rendering of navigation links

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,7 @@ import {useUserContext} from '../hooks/contextHooks';
 import {useEffect} from 'react';
 
 const Layout = () => {
-  const {handleAutoLogin} = useUserContext();
+  const {user, handleAutoLogin} = useUserContext();
   useEffect(() => {
     handleAutoLogin();
   }, []);
@@ -17,18 +17,23 @@ const Layout = () => {
             <li>
               <Link to="/">Home</Link>
             </li>
-            <li>
-              <Link to="/profile">Profile</Link>
-            </li>
-            <li>
-              <Link to="/upload">Upload</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/logout">Logout</Link>
-            </li>
+            {user ? (
+              <>
+                <li>
+                  <Link to="/profile">Profile</Link>
+                </li>
+                <li>
+                  <Link to="/upload">Upload</Link>
+                </li>
+                <li>
+                  <Link to="/logout">Logout</Link>
+                </li>
+              </>
+            ) : (
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            )}
           </ul>
         </nav>
       </header>
